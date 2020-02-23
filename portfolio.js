701d55caacaded2a46caeda007a26b849634a08c
CKconor <conorkemp@gmail.com>
2020-02-23 21:03:48 +0000

    Switched to Sass

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,32 @@ typewriter.typeString('Web Developer')
     .typeString('Creative')
     .pauseFor(2500)
     .start();
+
+// Add scrollspy to <body>
+$('body').scrollspy({ target: ".navbar", offset: 50 });
+
+// Add smooth scrolling on all links inside the navbar
+$("#mainNavbar a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+
+    } // End if
+
+});
